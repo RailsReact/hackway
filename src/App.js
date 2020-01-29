34605c817ead71d2128fd7a9c29ebe0c1b18bf2b
excavator <str_token@mail.ru>
2020-01-29 15:32:38 +0300

    hello with variables

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const helloWorld = 'Welcome to the Way React learn!'
+  const user = {name: 'Вася', lastname: 'Пупкин'}
   return (
     <div className="App">
-      <h2>Welcome to the Way React learn! </h2>
+      <h2> {helloWorld} </h2>
+      <h2>{user.name}</h2>
+      <h2>{user.lastname}</h2>
     </div>
   );
 }
