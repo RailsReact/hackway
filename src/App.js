aba1b62a74bfff600358b70dbf08099c9b863dbb
excavator <str_token@mail.ru>
2020-02-05 16:32:34 +0300

    use destructuring

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ class App extends React.Component  {
   }
 
   render(){
+    const {searchTerm, list}=this.state
     return (
       <div className="App">
         <form>
@@ -56,7 +57,7 @@ class App extends React.Component  {
             onChange={this.onSearchChange}
           />
         </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
+        {list.filter(isSearched(searchTerm)).map(item =>
           <div key={item.objecrsID}>
             <span>
               <a href={item.url}>{item.title}</a>
