e6ef6e1200db62567e916aaf241b631251d0bca3
excavator <str_token@mail.ru>
2020-02-15 15:04:21 +0300

    use children end

--- src/App.js
+++ src/App.js
@@ -71,8 +71,8 @@ class Search extends React.Component{
   render(){
     const {value, onChange, children } = this.props;
     return (
-      { children } <form>
-        <input 
+      <form>
+        { children } <input 
           type="text" 
           value={value}
           onChange={onChange}
